22b0a17ea49927bfb55c7c7caf687da6c886fa5b
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-11-04 00:12:30 +1100

    add route for single restaurant query

--- src/controllers/restaurant_controller.py
+++ src/controllers/restaurant_controller.py
@@ -5,12 +5,13 @@ from models.restaurant import Restaurant, RestaurantSchema
 restaurants_bp = Blueprint('restaurants', __name__, url_prefix='/restaurants')
 
 @restaurants_bp.route('/')
-# # @jwt_required()
 def all_restaurants():
-
     stmt = db.select(Restaurant)
     restaurants = db.session.scalars(stmt)
     return RestaurantSchema(many=True).dump(restaurants)
-#     return 'all restaurants'
 
-
+@restaurants_bp.route('/<int:id>/')
+def one_restaurants(id):
+    stmt = db.select(Restaurant).filter_by(id=id)
+    restaurant = db.session.scalar(stmt)
+    return RestaurantSchema().dump(restaurant)
