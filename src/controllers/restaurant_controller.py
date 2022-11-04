a21801bee3efdead51a6cce173e5d0fc6f1b46fd
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-11-04 20:59:26 +1100

    add route to post new restaurant

--- src/controllers/restaurant_controller.py
+++ src/controllers/restaurant_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from db import db
 from models.restaurant import Restaurant, RestaurantSchema
 
@@ -19,4 +19,16 @@ def one_restaurant(id):
     else:
         return {'error': f'Restaurant not found with id {id}'}, 404
 
-
+@restaurants_bp.route('/', methods=['POST'])
+def add_restaurant():
+    restaurant = Restaurant(
+        name = request.json['name'],
+        address = request.json['address'],
+        price_range = request.json['price_range'],
+        is_vegan = request.json['is_vegan']
+    )
+    #Add the restaurant to the database
+    db.session.add(restaurant)
+    db.session.commit()
+    #Returning a response with the new restaurant's info
+    return RestaurantSchema().dump(restaurant), 201
