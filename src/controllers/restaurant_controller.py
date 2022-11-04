30d1cbf5330b071905e918bc6701d0e81e21b1a5
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-11-04 21:18:11 +1100

    add route to show vegan friendly restaurants

--- src/controllers/restaurant_controller.py
+++ src/controllers/restaurant_controller.py
@@ -5,13 +5,19 @@ from models.restaurant import Restaurant, RestaurantSchema
 restaurants_bp = Blueprint('restaurants', __name__, url_prefix='/restaurants')
 
 @restaurants_bp.route('/')
-def all_restaurants():
+def get_all_restaurants():
     stmt = db.select(Restaurant)
     restaurants = db.session.scalars(stmt)
     return RestaurantSchema(many=True).dump(restaurants)
 
+@restaurants_bp.route('/vf/')
+def get_vegan_friendly_restaurants():
+    stmt = db.select(Restaurant).filter_by(is_vegan=True).order_by(Restaurant.price_range.desc())
+    restaurants = db.session.scalars(stmt)
+    return RestaurantSchema(many=True).dump(restaurants)
+
 @restaurants_bp.route('/<int:id>/')
-def one_restaurant(id):
+def get_one_restaurant(id):
     stmt = db.select(Restaurant).filter_by(id=id)
     restaurant = db.session.scalar(stmt)
     if restaurant:
