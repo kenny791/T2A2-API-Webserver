58fecec3042174309bd6f89886e26a0f1cbbe47a
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2022-11-06 00:31:17 +1100

    add errorhandler for missing fields

--- src/main.py
+++ src/main.py
@@ -14,6 +14,14 @@ def create_app():
     def not_found(err):
         return {'error': str(err)}, 404
 
+    @app.errorhandler(401)
+    def unauthorized(err):
+        return {'error': str(err)}, 401
+
+    @app.errorhandler(KeyError)
+    def key_error(err):
+        return {'error': f'The field {err} is required.'}, 400
+
     app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
